00920ec268a9ae8ff3b4347fbcf24cd994a87831
hillaryme <tenskap@gmail.com>
2022-06-30 15:28:43 +0300

    Added ingress-nginx service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
+import {Request, Response} from "express";
+
 const express = require('express');
 const { json } = require('body-parser');
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
+app.get('/api/users/currentuser', (req:Request, res:Response ) => {
     res.send('Hi there!');
 });
 
