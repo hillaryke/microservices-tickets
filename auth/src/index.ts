f51344e25809df182a17348f481bd1e93adcf95e
hillaryme <tenskap@gmail.com>
2022-06-30 15:06:55 +0300

    Added skaffold config for automatic deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const { json } = require('body-parser');
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!');
+});
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
 })
