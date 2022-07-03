2409b48ee49333cf6ec75928e0c0868b21bd065d
hillaryme <tenskap@gmail.com>
2022-07-03 12:57:41 +0300

    User creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
+import { User } from '../models/User';
 import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
 
 const express = require('express');
 const router = express.Router();
@@ -22,11 +22,19 @@ router.post('/api/users/signup', [
         throw new RequestValidationError(errors.array());
     }
 
+    const { email, password } = req.body;
 
-    console.log('Creating a user');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+        console.log('Email in use');
+        return res.send({ })
+    }
+
+    const user = User.build({ email, password})
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export {router as signupRouter};
