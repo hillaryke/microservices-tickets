bb531019ce8dc4582e4146962865c2363a802efb
hillaryme <tenskap@gmail.com>
2022-07-04 01:16:13 +0300

    using Current User middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,22 +1,14 @@
-import { Request, Response } from "express";
-import jwt from 'jsonwebtoken';
+import { currentUser } from "../middlewares/current-user";
 
 const express = require('express');
+import { Request, Response } from "express";
+
 const router = express.Router();
 
 // Get current user
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    if (!req.session?.jwt) {
-        return res.send({ currentUser: null });
-    }
+router.get('/api/users/currentuser', currentUser,(req: Request, res: Response) => {
 
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-
-        res.send({ currentUser: payload });
-    } catch (err) {
-        res.send({ currentUser: null });
-    }
+    res.send({ currentUser: req.currentUser || null });
 
 });
 
