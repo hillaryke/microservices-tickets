a00d9d2463153ebf28df3ebc7502fe035d74391c
hillaryme <tenskap@gmail.com>
2022-07-04 00:36:20 +0300

    Returning current User

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,11 +1,23 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import jwt from 'jsonwebtoken';
 
 const express = require('express');
 const router = express.Router();
 
 // Get current user
 router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    res.send('CURRENT USER');
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+        res.send({ currentUser: payload });
+    } catch (err) {
+        res.send({ currentUser: null });
+    }
+
 });
 
 export { router as currentUserRouter };
