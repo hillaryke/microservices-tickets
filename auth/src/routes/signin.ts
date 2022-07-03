e43fb0e5fb8bcbc01df93a03519c47d4eff26895
hillaryme <tenskap@gmail.com>
2022-07-03 19:18:55 +0300

    Signin in user

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,28 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import { body, validationResult } from 'express-validator';
+
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const express = require('express');
 const router = express.Router();
 
 // Get current user
-router.post('/api/users/signin', (req: Request, res: Response) => {
-    res.send('SIGN IN');
+router.post('/api/users/signin',
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Email must be valid!'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Field must not be empty!')
+],(req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array());
+    }
+
 });
 
 export { router as signinRouter };
