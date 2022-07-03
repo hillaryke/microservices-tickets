759a6740101730b07759f36153cea4bf4386e483
hillaryme <tenskap@gmail.com>
2022-07-04 00:51:49 +0300

    Signing out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/api/users/signout', (req: Request, res: Response) => {
     req.session = null;
 
-    res.send({ })
+    res.send({});
 });
 
 export { router as signoutRouter };
