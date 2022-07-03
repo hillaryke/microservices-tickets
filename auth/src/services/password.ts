b5fd7c4b13a192f73ae50d00271f904eee107000
hillaryme <tenskap@gmail.com>
2022-07-03 13:57:22 +0300

    Compare hashed password with supplied

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -7,13 +7,17 @@ const scryptAsync = promisify(scrypt);
 export class Password {
     static async toHash(password: string) {
         const salt = randomBytes(8).toString('hex');
-        const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
-        const hashedPassword = buffer.toString();
+        const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+        const hashedPassword = buf.toString('hex');
 
         return `${hashedPassword}.${salt}`;
     }
 
-    static compare(storedPassword: string, suppliedPassword: string) {
+    static async compare(storedPassword: string, suppliedPassword: string) {
+        const [storedHashedPassword, salt] = storedPassword.split('.');
+        const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+        const suppliedHashedPassword = buf.toString('hex');
 
+        return suppliedHashedPassword === storedHashedPassword;
     }
 }
