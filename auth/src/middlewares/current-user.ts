96a619f3976550780a3bbbb69e08ab67e106ab2a
hillaryme <tenskap@gmail.com>
2022-07-04 01:04:52 +0300

    Augmenting type definition of Express - adding object to interface

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,7 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-interface
+interface UserPayload {
+    id: string;
+    email: string
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload
+        }
+    }
+}
 
 export const currentUser = (req: Request, res: Response, next: NextFunction) => {
     if (!req.session?.jwt) {
@@ -9,11 +20,9 @@ export const currentUser = (req: Request, res: Response, next: NextFunction) => 
     }
 
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
         req.currentUser = payload;
-    } catch (err) {
-
-    }
+    } catch (err) {}
 
     next();
 };
