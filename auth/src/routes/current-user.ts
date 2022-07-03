3d3d83a3dfd4bb1d9a370b4a23edc14e63984b5b
hillaryme <tenskap@gmail.com>
2022-07-04 01:38:56 +0300

    clean-up

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,12 +2,11 @@ const express = require("express");
 import { Request, Response } from "express";
 
 import { currentUser } from "../middlewares/current-user";
-import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 // Get current user
-router.get("/api/users/currentuser", currentUser, requireAuth, (req: Request, res: Response) => {
+router.get("/api/users/currentuser", currentUser, (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
 });
 
