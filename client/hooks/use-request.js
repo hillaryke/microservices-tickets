ce441d6f3fde38597928a44b6bf3eaa55abc1898
hillaryme <tenskap@gmail.com>
2022-07-05 08:50:10 +0300

    Add onSuccess Callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useState } from "react";
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
    const [errors, setErrors] = useState([]);
 
    const doRequest = async () => {
       try {
          setErrors(null);
          const response = await axios[method](url, body);
+
+         if (onSuccess) {
+            onSuccess(response.data);
+         }
+
          return response.data;
       } catch (err) {
          setErrors(
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import useRequest from "../../hooks/use-request";
 
 const SignUp = () => {
@@ -10,7 +10,8 @@ const SignUp = () => {
       method: 'post',
       body: {
          email, password
-      }
+      },
+      onSuccess: () => Router.push('/')
    });
 
    const onFormSubmit = async (event) => {
