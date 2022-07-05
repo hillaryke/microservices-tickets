ac7b666a3cb6bf33f9216d027d9b1b073324e412
hillaryme <tenskap@gmail.com>
2022-07-05 15:29:34 +0300

    Determing if req is on Client or Browser in NEXTJS

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,31 @@
-export default () => {
-    return <h1>Landing Page</h1>
-}
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+   console.log(currentUser);
+
+   return <h1>Landing Page</h1>;
+};
+
+LandingPage.getInitialProps = async () => {
+   if (typeof window === "undefined") {
+      // we are on the server!
+      // http://service-name.namespace.svc.cluster.local/api/users
+
+      const { data } = await axios.get(
+          "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
+             headers: {
+                Host: "ticketing.dev"
+             }
+          }
+      );
+      return { currentUser: data };
+
+   } else {
+      // we are on the browser!
+      const { data } = await axios.get('/api/users/currentuser');
+
+      return { currentUser: data };
+   }
+};
+
+export default LandingPage;
