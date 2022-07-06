4d7871188044a1a58252b4fb5390d0bd2aae2bcb
hillaryme <tenskap@gmail.com>
2022-07-06 15:52:26 +0300

    Implement for input validation

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,18 +10,26 @@ import {
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
-   const ticket = await Ticket.findById(req.params.id);
+router.put('/api/tickets/:id',
+   [
+      body('title').not().isEmpty().withMessage('Title is required!'),
+      body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than zero')
+   ],
+   validateRequest,
+   requireAuth,
+   async (req: Request, res: Response) => {
+      const ticket = await Ticket.findById(req.params.id);
 
-   if (!ticket) {
-      throw new NotFoundError();
-   }
+      if (!ticket) {
+         throw new NotFoundError();
+      }
 
-   if (ticket.userId !== req.currentUser!.id) {
-      throw new NotAuthorizedError();
-   }
+      if (ticket.userId !== req.currentUser!.id) {
+         throw new NotAuthorizedError();
+      }
 
-   res.send(ticket);
-});
+
+      res.send(ticket);
+   });
 
 export { router as updateTicketRouter };
