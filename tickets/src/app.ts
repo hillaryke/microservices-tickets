253129904b152968332c340f01e1d4bf3eda07a5
hillaryme <tenskap@gmail.com>
2022-07-06 06:20:24 +0300

    First test creating ticket router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,10 @@
 const express = require('express');
-import 'express-async-errors';
-import { errorHandler, NotFoundError } from "@itickets/common";
-
 const { json } = require('body-parser');
+import 'express-async-errors';
 import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@itickets/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }));
+
+app.use(createTicketRouter);
 
 app.all('*', async () => {
     throw new NotFoundError();
