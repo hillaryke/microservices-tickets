312738c7d0e1cb5db0acd9e9faa8c7e244059c91
hillaryme <tenskap@gmail.com>
2022-07-06 14:27:02 +0300

    Fetch ticket via router Handler

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError, currentUser } from "@itickets/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 
@@ -13,16 +14,17 @@ app.set('trust proxy', true);
 
 app.use(json());
 app.use(cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+   signed: false,
+   secure: process.env.NODE_ENV !== 'test'
 }));
 
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
-    throw new NotFoundError();
+   throw new NotFoundError();
 });
 
 app.use(errorHandler);
