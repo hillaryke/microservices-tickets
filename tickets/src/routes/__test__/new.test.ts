c58e0191e58a97a04aad2e703e48fc2c726334b6
hillaryme <tenskap@gmail.com>
2022-07-06 13:14:33 +0300

    Tests for checking title and price inputs

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -11,19 +11,16 @@ it('has a route handler listening to /api/tickets for post requests', async () =
     expect(response.status).not.toEqual(404);
 });
 
-it('returns an error if an invalid title is provided', async () => {
+it('can only be accessed if the user is signed in', async () => {
     await request(app)
         .post('/api/tickets')
         .send({})
         .expect(401);
-
 });
 
-it('return status other than 401 if signed in', async () => {
+it('return a status other than 401 if user is signed in', async () => {
     jest.setTimeout(10 * 1000);
-
     const cookie = global.signin();
-
     const response = await request(app)
         .post('/api/tickets')
         .set('Cookie', cookie)
@@ -33,11 +30,9 @@ it('return status other than 401 if signed in', async () => {
 }, 10000);
 
 it('returns an error if an invalid title is provided', async () => {
-    const cookie = await global.signin();
-
     await request(app)
         .post('/api/tickets')
-        .set('Cookie', cookie)
+        .set('Cookie', global.signin())
         .send({
             title: '',
             price: 10
@@ -46,11 +41,32 @@ it('returns an error if an invalid title is provided', async () => {
 
     await request(app)
         .post('/api/tickets')
-        .set('Cookie', cookie)
+        .set('Cookie', global.signin())
         .send({
             price: 10
         })
         .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'AldjandD',
+            price: -10
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'AldjandD',
+        })
+        .expect(400);
+
+
 });
 
 it('creates a ticket with invalid inputs', async () => {
