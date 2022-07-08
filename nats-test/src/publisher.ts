ab32494d363272d5b0ad7c7269bcd2448d97ec8f
hillaryme <tenskap@gmail.com>
2022-07-08 12:56:16 +0300

    Using promise and Await event publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,24 +6,13 @@ const stan = nats.connect('ticketing', 'abc', {
    url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
    console.log('Publisher connected to NATS');
 
    const publisher = new TicketCreatePublisher(stan);
-   publisher.publish({
+   await publisher.publish({
       id: '123',
       title: 'concert',
       price: 20
-   })
-
-   //
-   // const data = JSON.stringify({
-   //    id: '123',
-   //    title: 'concert',
-   //    price: 20,
-   // });
-   //
-   // stan.publish('ticket:created', data, () => {
-   //    console.log('Event published');
-   // });
+   });
 });
