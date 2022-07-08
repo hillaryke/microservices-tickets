3d2d5df82a2d5cd488bd96633dd4affed630473a
hillaryme <tenskap@gmail.com>
2022-07-08 11:27:50 +0300

    Add Listener Abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from "crypto";
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -36,3 +36,54 @@ stan.on('connect', () => {
 
 process.on('SIGNINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+
+abstract class Listener {
+   abstract subject: string;
+   abstract queueGroupName: string;
+
+   abstract onMessage(data: any, msg: Message): void;
+
+   private client: Stan;
+   protected ackWait = 5 * 1000;
+
+   constructor(client: Stan) {
+      this.client = client;
+   }
+
+   subscriptionOptions() {
+      return this.client
+         .subscriptionOptions()
+         .setDeliverAllAvailable()
+         .setManualAckMode(true)
+         .setAckWait(this.ackWait)
+         .setDurableName(this.queueGroupName);
+   }
+
+   listen() {
+      const subscription = this.client.subscribe(
+         this.subject,
+         this.queueGroupName,
+         this.subscriptionOptions()
+      );
+
+      subscription.on('message', (msg: Message) => {
+         console.log(
+            `Message Recieved: ${this.subject} / ${this.queueGroupName}`
+         );
+
+         const parseData = this.parseMessage(msg);
+         this.onMessage(parseData, msg);
+      });
+   }
+
+   parseMessage(msg: Message) {
+      const data = msg.getData();
+
+      // if string or buffer
+      return typeof data === 'string'
+         ? JSON.parse(data)
+         : JSON.parse(data.toString('utf8'));
+
+   }
+}
