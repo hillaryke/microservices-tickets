508e2e71782893f0a086efd51f40021a9f0414ec
hillaryme <tenskap@gmail.com>
2022-07-08 13:22:22 +0300

    Updated Common Module for events

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,4 +1,4 @@
 export enum Subjects {
    TicketCreated = 'ticket:created',
-   OrderUpdated = 'order:updated'
+   TicketUpdated = 'ticket:updated'
 }
--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -1,10 +1,11 @@
 import { Subjects } from "./subjects";
 
-export interface TicketCreatedEvent {
-   subject: Subjects.TicketCreated;
+export interface TicketUpdatedEvent {
+   subject: Subjects.TicketUpdated;
    data: {
       id: string;
       title: string;
       price: number;
+      userId: string;
    };
 }
