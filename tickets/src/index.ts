02aebcef551a54ccf33369aee1284236160fd94e
hillaryme <tenskap@gmail.com>
2022-07-17 14:36:14 +0300

    Add NATS env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,13 +7,25 @@ const start = async () => {
    if (!process.env.JWT_KEY) {
       throw new Error('JWT_KEY must be defined');
    }
-
    if (!process.env.MONGO_URI) {
       throw new Error('MONGO_URI must be defined');
    }
+   if (!process.env.NATS_CLENT_ID) {
+      throw new Error('NATS_CLENT_ID must be defined');
+   }
+   if (!process.env.NATS_URL) {
+      throw new Error('NATS_URL must be defined');
+   }
+   if (!process.env.NATS_CLUSTER_ID) {
+      throw new Error('NATS_CLUSTER_ID must be defined');
+   }
 
    try {
-      await natsWrapper.connect('ticketing', 'laksdf', 'http://nats-svc:4222');
+      await natsWrapper.connect(
+         process.env.NATS_CLUSTER_ID,
+         process.env.NATS_CLIENT_ID!,
+         process.env.NATS_URL
+      );
       natsWrapper.client.on('close', () => {
          console.log('NATS connection closed!');
          process.exit();
