f6b35bdd56b64ff420bdab683567760919646df8
hillaryme <tenskap@gmail.com>
2022-07-17 14:22:43 +0300

    Ensuring mock invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 jest.useRealTimers();
 
@@ -87,6 +88,22 @@ it('creates a ticket with valid inputs', async () => {
    expect(tickets.length).toEqual(1);
    expect(tickets[0].price).toEqual(20);
    expect(tickets[0].title).toEqual(title);
+});
+
+it('publishes an event', async () => {
+
+   const title = 'NewTIcket';
+
+   await request(app)
+      .post('/api/tickets')
+      .set('Cookie', global.signin())
+      .send({
+         title,
+         price: 20
+      })
+      .expect(201);
+
+   expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
 
 
