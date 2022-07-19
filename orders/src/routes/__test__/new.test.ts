bac2d95db340512e8b906a07e137241dfc263867
hillaryme <tenskap@gmail.com>
2022-07-19 11:49:22 +0300

    Testing reserved tickets

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,5 +1,8 @@
 import request from 'supertest';
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { OrderStatus } from "@itickets/common";
+import { Order } from "../../models/order";
 
 const mongoose = require('mongoose');
 
@@ -18,7 +21,25 @@ it('returns an error if the ticket does not exist', async () => {
 });
 
 it('returns an error if the ticket is already reserved', async () => {
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 40
+   });
+   await ticket.save();
 
+   const order = Order.build({
+      ticket,
+      userId: 'lasdkjlfjasdljf',
+      status: OrderStatus.Created,
+      expiresAt: new Date()
+   });
+   await order.save();
+
+   await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ ticketId: ticket.id })
+      .expect(400);
 });
 
 it('reserves a ticket', async () => {
