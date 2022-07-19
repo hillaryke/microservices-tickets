1548af2be6d8a61f614fa19faa5051b3b40c73ae
hillaryme <tenskap@gmail.com>
2022-07-19 14:54:00 +0300

    Test event publishing

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it('marks an order as cancelled', async () => {
    // Create a ticket with Ticket Model
@@ -34,4 +35,29 @@ it('marks an order as cancelled', async () => {
 
 });
 
-it.todo('emits a order cancelled event');
+it('emits a order cancelled event', async () => {
+   // Create a ticket with Ticket Model
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 30
+   });
+   await ticket.save();
+
+   const user = global.signin();
+
+   // make request to create an order
+   const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', user)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+   // make request to canecl the order
+   await request(app)
+      .delete(`/api/orders/${order.id}`)
+      .set('Cookie', user)
+      .send()
+      .expect(204);
+
+   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
