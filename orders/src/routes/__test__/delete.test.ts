c052af7c2785584bd75f9f0224da67e972ce3795
hillaryme <tenskap@gmail.com>
2022-07-19 13:39:08 +0300

    Test Cancel Order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it('marks an order as cancelled', async () => {
+   // Create a ticket with Ticket Model
+   const ticket = Ticket.build({
+      title: 'concert',
+      price: 30
+   });
+   await ticket.save();
+
+   const user = global.signin();
+
+   // make request to create an order
+   const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', user)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+   // make request to canecl the order
+   await request(app)
+      .delete(`/api/orders/${order.id}`)
+      .set('Cookie', user)
+      .send()
+      .expect(204);
+
+   // expectation to make sure the thing is cancelled
+   const updatedOrder = await Order.findById(order.id);
+
+   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+
+});
+
+it.todo('emits a order cancelled event');
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from "../models/order";
+import { NotAuthorizedError, NotFoundError, requireAuth } from "@itickets/common";
 
 const router = express.Router();
 
-router.delete('/api/orders/:ordersID', async (req: Request, res: Response) => {
-   res.send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+   const { orderId } = req.params;
+
+   const order = await Order.findById(orderId);
+
+   if (!order) {
+      throw new NotFoundError();
+   }
+   if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+   }
+
+   order.status = OrderStatus.Cancelled;
+   await order.save();
+
+   // publising an event saying this was cancelled!
+
+   res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
