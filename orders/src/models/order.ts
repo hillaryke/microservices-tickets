1a4ea06b8b6eac6326b2f9c4afab40bb10e74a04
hillaryme <tenskap@gmail.com>
2022-07-19 10:32:34 +0300

    Fix typo

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -27,7 +27,7 @@ const orderSchema = new mongoose.Schema({
    status: {
       type: String,
       required: true,
-      enum: Object.Values(OrderStatus),
+      enum: Object.values(OrderStatus),
       default: OrderStatus.Created
    },
    expiresAt: {
