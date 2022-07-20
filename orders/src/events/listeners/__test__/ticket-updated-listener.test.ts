9b665a2125ad41d58e14f8059931a9339a817ec7
hillaryme <tenskap@gmail.com>
2022-07-20 08:00:31 +0300

    Test Out of Oder Events

--- orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -55,3 +55,16 @@ it('acks the message', async () => {
 
    expect(msg.ack).toHaveBeenCalled();
 });
+
+it('does not call ack if the event has a skipped version number', async () => {
+   const { msg, data, listener } = await setup();
+
+   data.version = 10;
+
+   try {
+      await listener.onMessage(data, msg);
+   } catch (err) {
+   }
+
+   expect(msg.ack).not.toHaveBeenCalled();
+});
