9df843a017736f93473659321dcd84620233e9fd
hillaryme <tenskap@gmail.com>
2022-07-20 08:26:58 +0300

    Add listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@itickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+   subject: Subjects.OrderCreated = Subjects.OrderCreated;
+   queueGroupName = queueGroupName;
+
+   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+      
+   }
+}
