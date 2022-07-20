f979e7306cfab68c4ba44ea8986ef141b5386b35
hillaryme <tenskap@gmail.com>
2022-07-20 17:19:53 +0300

    Adding job to Queue on Event arrival

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@itickets/common';
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
    subject: Subjects.OrderCreated = Subjects.OrderCreated;
    queueGroupName = queueGroupName;
 
    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+      await expirationQueue.add({
+         orderId: data.id
+      });
 
+      msg.ack();
    }
 }
