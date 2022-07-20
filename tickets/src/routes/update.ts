dd3ac9313515e79ed59d63cc67d9a7d842432812
hillaryme <tenskap@gmail.com>
2022-07-20 20:57:39 +0300

    rearrange code

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -13,15 +13,13 @@ import { natsWrapper } from "../nats-wrapper";
 const router = express.Router();
 
 router.put('/api/tickets/:id',
+   requireAuth,
    [
       body('title').not().isEmpty().withMessage('Title is required!'),
       body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than zero')
    ],
    validateRequest,
-   requireAuth,
    async (req: Request, res: Response) => {
-      const { title, price } = req.body;
-
       const ticket = await Ticket.findById(req.params.id);
 
       if (!ticket) {
@@ -36,9 +34,13 @@ router.put('/api/tickets/:id',
          throw new NotAuthorizedError();
       }
 
-      ticket.set({ title, price });
+      ticket.set({
+         title: req.body.title,
+         price: req.body.price
+      });
 
       await ticket.save();
+
       new TicketUpdatedPublisher(natsWrapper.client).publish({
          id: ticket.id,
          title: ticket.title,
