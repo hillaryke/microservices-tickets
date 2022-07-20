7364437a5dc5421da4db6404e1457701478370b2
hillaryme <tenskap@gmail.com>
2022-07-20 10:26:54 +0300

    fix warnings in webstorm

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -6,7 +6,7 @@ import { natsWrapper } from "../../nats-wrapper";
 
 it('returns a 404 if the provided id does not exist', async () => {
    const id = new mongoose.Types.ObjectId().toHexString();
-   const response = await request(app)
+   await request(app)
       .put(`/api/tickets/${id}`)
       .set("Cookie", global.signin())
       .send({
@@ -18,7 +18,7 @@ it('returns a 404 if the provided id does not exist', async () => {
 
 it('returns a 401 if the user is not authenticated', async () => {
    const id = new mongoose.Types.ObjectId().toHexString();
-   const response = await request(app)
+   await request(app)
       .put(`/api/tickets/${id}`)
       .send({
          title: 'concert',
