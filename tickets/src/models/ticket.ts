507b46f0bee06a3789a5458fd8df83def41026de
hillaryme <tenskap@gmail.com>
2022-07-20 08:35:40 +0300

    Setup for locking a Ticket

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -11,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
    price: number;
    userId: string;
    version: number;
+   orderId?: string;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -29,6 +30,9 @@ const ticketSchema = new mongoose.Schema({
    userId: {
       type: String,
       required: true
+   },
+   orderId: {
+      type: String
    }
 }, {
    toJSON: {
