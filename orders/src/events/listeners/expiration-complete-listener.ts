f334db2f3c59ba09000cfc473137708c8f3ec5e0
hillaryme <tenskap@gmail.com>
2022-07-21 18:14:18 +0300

    Emit Order Cancelled Event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,6 +2,8 @@ import { ExpirationCompleteEvent, Listener, NotFoundError, OrderStatus, Subjects
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
@@ -9,16 +11,28 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
    async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
       // Find order with expiration complete
-      const order = await Order.findById(data.orderId);
+      const order = await Order.findById(data.orderId).populate('ticket');
 
       // Check if order exists
       if (!order) {
          throw new NotFoundError();
       }
 
-      // Update Status of order to cancelled
+      // Update Status of order to cancelled and save
       order.set({
          status: OrderStatus.Cancelled
       });
+      await order.save();
+
+      // Emit event saying order is cancelled
+      await new OrderCancelledPublisher(natsWrapper.client).publish({
+         id: order.id,
+         version: order.version,
+         ticket: {
+            id: order.ticket.id
+         }
+      });
+
+      msg.ack();
    }
 }
