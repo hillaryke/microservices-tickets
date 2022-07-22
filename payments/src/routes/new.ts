cd1cc3af9217eca676cc0b20c5bc4a7bc0e56c4b
hillaryme <tenskap@gmail.com>
2022-07-22 17:08:00 +0300

    Validate payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError } from "@itickets/common";
+import {
+   requireAuth,
+   validateRequest,
+   BadRequestError,
+   NotFoundError,
+   NotAuthorizedError,
+   OrderStatus
+} from "@itickets/common";
 import { Order } from "../models/order";
 
 const router = express.Router();
@@ -13,6 +20,25 @@ router.post('/api/payments',
    ],
    validateRequest,
    async (req: Request, res: Response) => {
+      const { token, orderId } = req.body();
+
+      // Find order associated with the charge
+      const order = await Order.findById(orderId);
+
+      if (!order) {
+         throw new NotFoundError();
+      }
+
+      // check if user requesting payment matches the user assocaited with order
+      if (order.userId !== req.currentUser!.id) {
+         throw new NotAuthorizedError();
+      }
+
+      // make sure order is not cancelled
+      if (order.status === OrderStatus.Cancelled) {
+         throw new BadRequestError('Cannot process payments for cancelled order!');
+      }
+
       res.send({ success: true });
    });
 
