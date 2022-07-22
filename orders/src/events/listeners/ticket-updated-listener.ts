2bf2fb7889dd094caa42f902f5eafd758f61ad5d
hillaryme <tenskap@gmail.com>
2022-07-22 07:33:14 +0300

    Update Version for OCC for existing ticket

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,39 +8,32 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
    queueGroupName = queueGroupName;
 
    async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-      console.log('=======TUL 1=========', data);
       const { title, price } = data;
-
       const filterQuery = {
          id: data.id,
          version: data.version - 1
       };
       const ticket = await Ticket.findOne(filterQuery);
 
-      console.log('==========TUL 1.1========', ticket);
       if (!ticket) {
          throw new Error('Ticket not found');
       }
 
       const existingTicket = await Ticket.exists({ title, price });
 
-      if (existingTicket) {
-         console.log('====== TUL --1.2 UPDATING VERSION ======');
+      if (!existingTicket) {
+         // Update ticket given new title or price values, then save it
+         ticket.set({ title, price });
+         await ticket.save();
+      } else {
+         // Only update version for existing ticket
+         // since mongoose OCC *on save* cannot do that when values given are unchanged
          await Ticket.findOneAndUpdate(filterQuery,
             {
                $inc: { version: 1 }
             }
          );
-      } else {
-         // Update ticket given new title or price values, then save it
-         ticket.set({ title, price });
-         await ticket.save();
       }
-
-      const updatedTicket = await Ticket.findById(data.id);
-
-      console.log('=======TUL 2 FINALUPATED=========', updatedTicket);
-
 
       msg.ack();
    }
