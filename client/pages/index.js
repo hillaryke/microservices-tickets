e5db237a7e88a425232433897efdadfe5e9d8082
hillaryme <tenskap@gmail.com>
2022-07-25 12:32:20 +0300

    Quick fixes in fetching

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,11 @@
-import buildClient from "../../api/build-client";
-
 const LandingPage = ({ currentUser }) => {
    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 
    return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
-   console.log('LANDING PAGE!');
-   const client = buildClient(context);
-   const { data } = await client.get('/api/users/currentuser');
-
-   return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+   return {};
 };
 
 export default LandingPage;
