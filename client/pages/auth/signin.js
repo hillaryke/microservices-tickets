87417c0cad9df272e956ec875079056c9bf7ebee
hillaryme <tenskap@gmail.com>
2022-07-31 15:36:09 +0300

    add tailwind in auth forms

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -21,74 +21,89 @@ const SignUp = () => {
       doRequest();
    };
 
+
    return (
-       <div className="flex min-h-full flex-col justify-center">
-          <div className="mx-auto w-full max-w-md px-8">
-             <form onSubmit={onFormSubmit} method="post" className="space-y-6">
-                <div>
-                   <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                      Email address
-                   </label>
-                   <div className="mt-1">
-                      <input
-                          value={email}
-                          onChange={e => setEmail(e.target.value)}
-                          id="inputEmail"
-                          required
-                          autoFocus={true}
-                          type="email"
-                          autoComplete="email"
-                          placeholder="name@example.com"
-                          className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                      />
-                   </div>
-                </div>
+       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+          </div>
 
-                <div>
-                   <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                      Password
-                   </label>
-                   <div className="mt-1">
-                      <input
-                          id="inputPassword"
-                          type="password"
-                          className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                      />
-                   </div>
-                </div>
-
-                <button
-                    type="submit"
-                    className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-                >
-                   Log in
-                </button>
-                <div className="flex items-center justify-between">
-                   <div className="flex items-center">
-                      <input
-                          id="remember"
-                          name="remember"
-                          type="checkbox"
-                          className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                      />
-                      <label
-                          htmlFor="remember"
-                          className="ml-2 block text-sm text-gray-900"
-                      >
-                         Remember me
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form onSubmit={onFormSubmit} className="space-y-6" action="#" method="POST">
+                   <div>
+                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                         Email address
                       </label>
+                      <div className="mt-1">
+                         <input
+                             value={email}
+                             onChange={e => setEmail(e.target.value)}
+                             id="email"
+                             name="email"
+                             type="email"
+                             autoComplete="email"
+                             required
+                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                         />
+                      </div>
                    </div>
+
+                   <div>
+                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                         Password
+                      </label>
+                      <div className="mt-1">
+                         <input
+                             value={password}
+                             onChange={e => setPassword(e.target.value)}
+                             id="password"
+                             name="password"
+                             type="password"
+                             autoComplete="current-password"
+                             required
+                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                         />
+                      </div>
+                   </div>
+
+                   <div className="flex items-center justify-between">
+                      <div className="flex items-center">
+                         <input
+                             id="remember-me"
+                             name="remember-me"
+                             type="checkbox"
+                             className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                         />
+                         <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                            Remember me
+                         </label>
+                      </div>
+
+                      <div className="text-sm">
+                         <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                            Forgot your password?
+                         </a>
+                      </div>
+                   </div>
+
+                   <div>
+                      <button
+                          type="submit"
+                          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                         Sign in
+                      </button>
+                   </div>
+
                    <div className="text-center text-sm text-gray-500">
                       Don't have an account?{" "}
-                      <Link
-                          className="text-blue-500 underline"
-                          href="/auth/signup"
-                      >
-                         Sign up
+                      <Link href="/auth/signup">
+                         <a className="text-blue-500 underline"> Sign up</a>
                       </Link>
                    </div>
-                </div>
-             </form>
+                </form>
+             </div>
           </div>
        </div>
    );
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -22,45 +22,65 @@ const SignUp = () => {
    };
 
    return (
-       <div>
-          <div>
-             <h5>Sign Up</h5>
+       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign Up</h2>
+          </div>
 
-             <form onSubmit={onFormSubmit}>
-                <div>
-                   <input
-                       value={email}
-                       onChange={e => setEmail(e.target.value)}
-                       type="email"
-                       id="inputEmail"
-                       placeholder="name@example.com"
-                   />
-                   <label htmlFor="inputEmail">Email address</label>
-                </div>
+          <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+             <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form onSubmit={onFormSubmit} className="space-y-6" action="#" method="POST">
+                   <div>
+                      <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                         Email address
+                      </label>
+                      <div className="mt-1">
+                         <input
+                             value={email}
+                             onChange={e => setEmail(e.target.value)}
+                             id="email"
+                             name="email"
+                             type="email"
+                             autoComplete="email"
+                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                         />
+                      </div>
+                   </div>
 
-                <div>
-                   <input
-                       value={password}
-                       onChange={e => setPassword(e.target.value)}
-                       type="password"
-                       id="inputPassword"
-                       placeholder="Password"
-                   />
-                   <label htmlFor="inputPassword">Password</label>
-                </div>
+                   <div>
+                      <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                         Password
+                      </label>
+                      <div className="mt-1">
+                         <input
+                             value={password}
+                             onChange={e => setPassword(e.target.value)}
+                             id="password"
+                             name="password"
+                             type="password"
+                             autoComplete="current-password"
+                             className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                         />
+                      </div>
+                   </div>
 
-                {errors}
+                   <div>
+                      <button
+                          type="submit"
+                          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                         Sign Up
+                      </button>
+                   </div>
 
-                <div>
-                   <button
-                       type="submit">Sign Up
-                   </button>
-                </div>
-
-                <Link href="/auth/signin">
-                   <a>Have an account? Sign In</a>
-                </Link>
-             </form>
+                   <div className="text-center text-sm text-gray-500">
+                      Already have an account?{" "}
+                      <Link href="/auth/signup">
+                         <a className="text-blue-500 underline"> Sign in</a>
+                      </Link>
+                   </div>
+                </form>
+             </div>
           </div>
        </div>
    );
