a9b215e8e720eb5e67808b3d0b92e8b56dfe0290
hillaryme <tenskap@gmail.com>
2022-07-31 23:51:26 +0300

    Set timer for order payment

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,31 @@
-const OrderShow = () => {
-   return <div>OrderShow</div>;
+import { useEffect, useState } from "react";
+
+const OrderShow = ({ order }) => {
+   const [timeLeft, setTimeLeft] = useState('');
+
+   useEffect(() => {
+      const findTimeLeft = () => {
+         const msLeft = new Date(order.expiresAt) - new Date();
+         const secondsLeft = msLeft / 1000;
+         setTimeLeft(Math.round(secondsLeft));
+      };
+
+      findTimeLeft();
+      const timerId = setInterval(findTimeLeft, 1000);
+
+      return () => {
+         clearInterval(timerId);
+      };
+   }, []);
+
+   return <div>{timeLeft} remaining to pay</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+   const { orderId } = context.query;
+   const { data } = await client.get(`/api/orders/${orderId}`);
+
+   return { order: data };
 };
 
 export default OrderShow;
